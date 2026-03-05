30e56d101cc3ec1acbbeb1a3690f062f100b5a71
Rohan Eapen <rohan.j.eapen@gmail.com>
2026-03-05 12:33:00 -0500

    added period

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -186,7 +186,7 @@ function Header({ isFundraiserPage = false, isEventPage = false, isArticlePage =
                         window.location.href = ORIGIN;
                 }}>
                     <h2 className="header-title">PRSM Allergy Foundation {isDashboardPage || isDashboardEventsPage || isDashboardFundraisersPage || isDashboardTeamPage || isDashboardArticlesPage ? "Dashboard" : ""}</h2>
-                    <h3 className="header-subtitle">Accelerating progress in allergy and immune health</h3>
+                    <h3 className="header-subtitle">Accelerating progress in allergy and immune health.</h3>
                 </div>
                 <nav className="header-nav">
                     {menuItems.map(item => (
